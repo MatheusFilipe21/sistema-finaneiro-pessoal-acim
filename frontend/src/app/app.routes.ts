fa7924991f05403305e19570aeee9324bf64e977
Matheus Pereira <matheusfnpereira@gmail.com>
2025-11-13 23:48:29 +0000

    refactor(SFP-7): Remove "Olá Mundo" (componente e serviço) para preparar a implementação do cadastro

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,8 @@
 import { Routes } from '@angular/router';
-import { OlaMundo } from './components/ola-mundo/ola-mundo';
 
 /**
  * Define as rotas principais da aplicação.
  *
  * @author Matheus F. N. Pereira
  */
-export const routes: Routes = [
-  {
-    path: '',
-    component: OlaMundo,
-  },
-];
+export const routes: Routes = [];
